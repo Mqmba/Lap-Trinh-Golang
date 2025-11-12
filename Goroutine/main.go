7ee521572f60845d450f17056afb51837a170a2a
Mqmba <nguyenhoanghuy9a5@gmail.com>
2025-11-12 21:35:58 +0700

    Refactor Goroutine implementation to use context for task management
    - Replaced the previous task function with an employee function that utilizes context for cancellation and priority handling.
    - Removed unused synchronization code and simplified the main function to demonstrate context usage.
    - Introduced context with a timeout and a priority value for better task management.

--- Goroutine/main.go
+++ Goroutine/main.go
@@ -1,43 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"sync"
 	"time"
 )
 
-func task(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("Task %d bắt đầu \n", id)
-	time.Sleep(1 * time.Second)
-	fmt.Printf("Task %d kết thúc \n", id)
+func employee(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Công việc đã bị hủy: ", ctx.Err())
+			return
+		default:
+			priority := ctx.Value("priority")
+			fmt.Println("Đang làm việc của task ưu tiên với mức độ: ", priority)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }
 
 func main() {
-	// // defer chạy cuối cùng
-	// defer fmt.Println("Kết thúc")
-	// fmt.Println("Bắt đầu")
-	// fmt.Println("Running 1")
-	// fmt.Println("Running 2")
-	// fmt.Println("Running 3")
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
 
-	// start := time.Now()
-	// // 4 task chạy cùng lúc khi sử dụng goroutine
-	// go task(1)
-	// go task(2)
-	// go task(3)
-	// go task(4)
+	ctx = context.WithValue(ctx, "priority", "high")
 
-	// time.Sleep(2 * time.Second)
-	start := time.Now()
-	var wg sync.WaitGroup
+	go employee(ctx)
 
-	for i := 1; i <= 4; i++ {
-		wg.Add(1)
-		go task(i, &wg)
-	}
-
-	wg.Wait()
-
-	fmt.Println("Tong thời gian hoàn thành: ", time.Since(start))
+	time.Sleep(3 * time.Second)
 }
